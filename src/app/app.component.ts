db2f587d346342555997b0d5ead8f866645559f6
Hang <hangnt036@gmail.com>
2022-10-12 15:34:24 +0700

    ng-template_and_ng-container: using ng-template, ngTemplateOutlet and ng-container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,43 +6,14 @@ import {
   ViewChild,
   ViewChildren,
 } from '@angular/core';
-import { ToggleComponent } from './toggle/toggle.component';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  // @ViewChild('toggleComp') toggleComp: ToggleComponent;
-  // @ViewChild(ToggleComponent, { static: true }) toggleComp: ToggleComponent;
-  @ViewChildren(ToggleComponent) toggleComp: QueryList<ToggleComponent>;
-
-  @ViewChild('nameInput') nameInput: ElementRef<HTMLInputElement>;
-
   title = 'ng-learning-angular';
-  checked: boolean = true;
-  showLast: boolean = true;
 
-  name = 'Hang Ngo';
-  questions = {
-    question1: true,
-    question2: false,
-  };
-
-  ngOnInit() {
-    setTimeout(() => {
-      this.nameInput.nativeElement.focus();
-    }, 3000);
-    // console.log('onInit', this.toggleComp);
-  }
-
-  ngAfterViewInit() {
-    console.log(this.nameInput);
-    // apiCall.subscribe(() => {
-    //   this.toggleComp.toggle();
-    // });
-
-    this.toggleComp.changes.subscribe(console.log);
-  }
+  counter = 1;
+  ngOnInit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,9 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AuthorsListComponent } from './authors/authors-list.component';
-import { AuthorsDetailsComponent } from './authors/authors-details.component';
-import { AuthorsComponent } from './authors/authors.component';
-import { ToggleComponent } from './toggle/toggle.component';
-import { HelloComponent } from './hello/hello.component';
-import { FlexComponent } from './flex/flex.component';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    AuthorsComponent,
-    AuthorsListComponent,
-    AuthorsDetailsComponent,
-    ToggleComponent,
-    HelloComponent,
-    FlexComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
